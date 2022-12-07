baf6bf0259aaabc4068897b2a3e98759f41e333e
saba000 <hanyubeiwai@gmail.com>
2022-12-07 18:47:27 +0900

    Test create custom row test

---
+++ src/test/java/com/ssr/image/downloader/ui/table/ImageTableTest.java
@@ -0,0 +1,22 @@
+package com.ssr.image.downloader.ui.table;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import org.junit.jupiter.api.Test;
+
+public class ImageTableTest {
+
+    @Test
+    void addCustomRow() {
+        var frame = new JFrame();
+        var model = new CustomTableModel();
+        var table = new JTable(model);
+        frame.add(new JScrollPane(table));
+        frame.setSize(500, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        System.out.println();
+    }
+}
