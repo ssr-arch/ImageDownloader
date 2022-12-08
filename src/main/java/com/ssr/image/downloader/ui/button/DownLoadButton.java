6ec02f2f33b4067c61c8606d997abb1458775707
saba000 <hanyubeiwai@gmail.com>
2022-12-08 12:06:15 +0900

    Add action of download

--- src/main/java/com/ssr/image/downloader/ui/button/DownLoadButton.java
+++ src/main/java/com/ssr/image/downloader/ui/button/DownLoadButton.java
@@ -2,18 +2,37 @@ package com.ssr.image.downloader.ui.button;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 
-import com.ssr.image.downloader.delegator.ReadRecordsDelegator;
+import com.ssr.image.downloader.delegate.ReadCheckedRecordsDelegate;
+import com.ssr.image.downloader.model.TableRecord;
+import com.ssr.image.downloader.ui.dialog.DownLoadDialog;
+import com.ssr.image.downloader.worker.DownLoadImageWorker;
 
 public class DownLoadButton {
 
     private final JButton button;
 
-    public DownLoadButton(ReadRecordsDelegator readRowsDelegator) {
+    public DownLoadButton(ReadCheckedRecordsDelegate readCheckedRowsDelegator) {
         this.button = new JButton("download");
         button.addActionListener(e -> {
-            var tableRecords = readRowsDelegator.read();
-            tableRecords.forEach(System.out::println);
+            var sources = readCheckedRowsDelegator.invoke()
+                    .stream()
+                    .map(TableRecord::getSource)
+                    .toList();
+            if (sources.size() == 0) {
+                JOptionPane.showMessageDialog(null,
+                        "not selected",
+                        "download",
+                        JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            var dialog = new DownLoadDialog();
+            var worker = new DownLoadImageWorker(sources, dialog.getShowDownloadFileNameDelegate());
+            worker.addPropertyChangeListener(dialog.getProgressBarCreator());
+            worker.addPropertyChangeListener(dialog.getWorkerCompletionWaiter());
+            worker.execute();
+            dialog.show();
         });
     }
 
